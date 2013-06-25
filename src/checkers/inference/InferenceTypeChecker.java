ef500ebc6f0fe3080865ead53af9aa96ef6aef16
jburke <jburke@jonathan-macbook.dyn.cs.washington.edu>
2013-06-25 15:28:57 -0700

    Add method comment for needsAnnotation

--- src/checkers/inference/InferenceTypeChecker.java
+++ src/checkers/inference/InferenceTypeChecker.java
@@ -21,6 +21,12 @@ public interface InferenceTypeChecker {
     // from BaseTypeChecker
     Set<Class<? extends Annotation>> getSupportedTypeQualifiers();
 
+    /**
+     * Should an annotation ever be applied to the given type ty.  Returning false from this method
+     * generally has the effect of removing it from the set of variables referenced by the inference
+     * @param ty type to check
+     * @return whether or not ty will ever need a type annotation
+     */
     boolean needsAnnotation(AnnotatedTypeMirror ty);
 
     AnnotationMirror defaultQualifier();
