0e090c71c2e8a229c32e3bdb85f162ef6293c219
jburke <jburke@jonathan-macbook.dyn.cs.washington.edu>
2013-07-31 15:16:04 -0700

    Add documentation to Inference quals.

--- src/checkers/inference/quals/CombVarAnnot.java
+++ src/checkers/inference/quals/CombVarAnnot.java
@@ -6,6 +6,12 @@ import checkers.quals.SubtypeOf;
 import checkers.quals.TypeQualifier;
 import checkers.quals.Unqualified;
 
+/**
+ * CombVarAnnot represents variables which are a combination of other variables, literals, or constants.  These
+ * annotations identify a unique CombVar object stored in the list combVariables in the SlotManager
+ * See @VarAnnot
+ */
+
 @Retention(RetentionPolicy.RUNTIME)
 @TypeQualifier
 @SubtypeOf({Unqualified.class})
