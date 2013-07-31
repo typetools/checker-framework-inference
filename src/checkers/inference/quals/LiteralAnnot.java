1ca1492696f5fed95a8d6017229ecf5316b77c01
jburke <jburke@jonathan-macbook.dyn.cs.washington.edu>
2013-07-31 15:16:04 -0700

    Add documentation to Inference quals.

--- src/checkers/inference/quals/LiteralAnnot.java
+++ src/checkers/inference/quals/LiteralAnnot.java
@@ -7,6 +7,13 @@ import checkers.quals.TypeQualifier;
 import checkers.quals.Unqualified;
 import com.sun.source.tree.Tree.Kind;
 
+/**
+ * Literal annotations are used to represent literal values as well as receivers whose values are the
+ * "this" reference (either implied or excplicitly written).  Literal annotations are converted into
+ * one of the Literal slot types but are NOT stored in SlotMgr like other slots.
+ * See @VarAnnot
+ */
+
 @Retention(RetentionPolicy.SOURCE)
 @TypeQualifier
 @SubtypeOf({ Unqualified.class })
