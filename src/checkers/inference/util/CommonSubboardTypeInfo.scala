84260d0d4eadc077b08f5109f4e696940bd84ac4
jonathangburke <jonathangburke@gmail.com>
2013-11-21 18:58:17 -0800

    Fix-up raw type information when generating subboard calls

--- src/checkers/inference/util/CommonSubboardTypeInfo.scala
+++ src/checkers/inference/util/CommonSubboardTypeInfo.scala
@@ -13,12 +13,26 @@ case class CommonSubboardTypeInfo(
   classTypeParamUBs   : List[AnnotatedTypeMirror],
   args : List[AnnotatedTypeMirror],
   results : Option[AnnotatedTypeMirror]
-)
+) {
+  val summary =
+    List( "receiver"           -> receiver,
+          "methodTypeParamLBs" -> methodTypeParamLBs,
+          "classTypeParamLBs"  -> classTypeParamLBs,
+          "methodTypeParamUBs" -> methodTypeParamUBs,
+          "classTypeParamUBs"  -> classTypeParamUBs,
+          "args"    -> args,
+          "results" -> results )
+      .map( {case (label, value) => label + ": " + value} )
+      .mkString("\n")
+
+  "CommonSubboardTypeInfo(\n" + summary + "\n)\n"
+}
 
 
 object CommonSubboardTypeInfo {
   private lazy val infChecker = InferenceMain.inferenceChecker
   private lazy val slotMgr    = InferenceMain.slotMgr
+  private lazy val log = new Log( classOf[CommonSubboardTypeInfo], InferenceMain.LogSettings )
 
 
   def makeCall[CONTEXT_VP <: WithinClassVP](
@@ -27,6 +41,7 @@ object CommonSubboardTypeInfo {
                             declared : CommonSubboardTypeInfo, called : CommonSubboardTypeInfo ) = {
 
     try {
+
       //TODO: Need to do more for wildcards
       val methodTypeParamLBs = called.methodTypeParamLBs.map( slotMgr.extractSlot _ )
       val classTypeParamLBs  = called.classTypeParamLBs.map(  slotMgr.extractSlot _ )
@@ -36,8 +51,11 @@ object CommonSubboardTypeInfo {
       //At the moment we only care about the 1st (primary) annotation on receivers
       val ( recvResult, recvSlots ) = getInputSlots( declared.receiver, called.receiver, subtypingVisitor )
 
+      val ( declaredClassTypeUBs,  calledClassTypeUBs  ) =
+        fixRawness( contextVp, calledVp, stubUse, declared.classTypeParamUBs,  called.classTypeParamUBs )
+
       val ( mtUBResult, methodTypeParamUBs ) = getInputSlots( declared.methodTypeParamUBs, called.methodTypeParamUBs, subtypingVisitor )
-      val ( clUBResult, classTypeParamUBs  ) = getInputSlots( declared.classTypeParamUBs, called.classTypeParamUBs, subtypingVisitor )
+      val ( clUBResult, classTypeParamUBs  ) = getInputSlots( declaredClassTypeUBs, calledClassTypeUBs, subtypingVisitor )
 
       subtypingVisitor.typeUse = true
       val ( argsResult, argsAsUb ) = getInputSlots( declared.args, called.args, subtypingVisitor )
@@ -58,13 +76,9 @@ object CommonSubboardTypeInfo {
                              subtypingResult.equality, subtypingResult.lowerBounds )
     } catch {
       case throwable : Throwable =>
-        val fieldSummaries =
-          List( "Context Pos"  -> contextVp, "Called Pos" -> calledVp, "StubUse" -> stubUse,
-                "Declared" -> declared, "Called" -> called )
-            .map({case (title, obj) => title +":\n" + obj })
-            .mkString("\n")
+        val fieldSummaries = summarizeFields( contextVp, calledVp, stubUse, declared, called )
 
-        throw new RuntimeException( "Throwable caught while making call to: \n" + fieldSummaries, throwable )
+        throw new RuntimeException( "Throwable caught while making call to: \n" + fieldSummaries + "\n", throwable )
     }
   }
 
@@ -78,7 +92,7 @@ object CommonSubboardTypeInfo {
 
   private def getInputSlots( declareds : List[AnnotatedTypeMirror], subtypes : List[AnnotatedTypeMirror], subtypingVisitor : SubtypingVisitor ) : (SubtypingResult, List[List[Slot]]) = {
 
-    assert( declareds.length == subtypes.length )
+    assert( declareds.length == subtypes.length, "Number of parameters and number of arguments don't match." )
     if( declareds.size == 0 ) {
       return ( SubtypingResult.empty, List.empty[List[Slot]] )
     }
@@ -107,5 +121,35 @@ object CommonSubboardTypeInfo {
     val accumulatedResult = subtypingResults.tail.foldLeft( subtypingResults.head ) ( _ merge _ )
 
     ( accumulatedResult , subtypesAsUb )
+  }
+
+  //NOTE: Everything besides declareds and subtypes is passed in solely for reporting reasons
+  private def fixRawness( contextVp : WithinClassVP, calledVp : Option[VariablePosition],
+                          stubUse : Option[StubBoardUseConstraint],
+                          declareds : List[AnnotatedTypeMirror], subtypes : List[AnnotatedTypeMirror] ) :
+    (List[AnnotatedTypeMirror], List[AnnotatedTypeMirror]) = {
+
+    ( declareds.isEmpty, subtypes.isEmpty ) match {
+      case (false, false ) | (true, true ) => ( declareds, subtypes )
+      case (false, true  ) =>
+        log.error("Raw type encountered: " +  summarizeFields( contextVp, calledVp, stubUse, declareds, subtypes ) )
+        ( declareds, declareds )
+      case (true,  false ) =>
+        log.error("Raw type encountered: " +  summarizeFields( contextVp, calledVp, stubUse, declareds, subtypes ) )
+        ( subtypes,  subtypes  )
+    }
+
+  }
+
+  private def summarizeFields( contextVp : WithinClassVP, calledVp : Option[VariablePosition],
+                               stubUse : Option[StubBoardUseConstraint],  declared : Object,
+                               called : Object ) = {
+    List( "Context Pos"  -> contextVp,
+          "Called Pos"   -> calledVp,
+          "StubUse"     -> stubUse,
+          "Declared"    -> declared,
+          "Called"      -> called )
+          .map({case (title, obj) => title +":\n" + obj })
+          .mkString("\n")
   }
 }
