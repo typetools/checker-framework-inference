ac2687c27afa02907c46f08ae49145c25c1de259
jonathangburke <jonathangburke@gmail.com>
2013-11-24 16:06:00 -0800

    Remove listDeclVariables and instead use SubtypingVisitor.listSlots.  Wildcard related changes: Add wildcard support to the SubtypingVisitor, better handle type-uses.  Return types on called methods are now lifted up to the declared methods return type before extracting slots.

--- src/checkers/inference/util/SlotUtil.scala
+++ src/checkers/inference/util/SlotUtil.scala
@@ -1,120 +1,14 @@
 package checkers.inference.util
 
 import checkers.types.AnnotatedTypeMirror
-import checkers.inference.{InferenceMain, Slot}
+import checkers.inference.{InferenceAnnotatedTypeFactory, InferenceMain, Slot}
 import scala.collection.mutable.ListBuffer
 import checkers.inference.InferenceMain._
 import checkers.types.AnnotatedTypeMirror._
 import java.util.{LinkedHashMap => JLinkedMap}
-import javax.lang.model.element.TypeParameterElement
+import javax.lang.model.element.{AnnotationMirror, TypeParameterElement}
 
 object SlotUtil {
-
-  def listSecondaryDeclVariables( atm : AnnotatedTypeMirror ) : (List[Slot], List[Slot]) = {
-    val primary   = new ListBuffer[Slot]
-    val secondary = new ListBuffer[Slot]
-    listDeclVariables( atm, Some( primary ), secondary )
-
-    ( primary.toList, secondary.toList )
-  }
-
-  def topToNestedDeclVariables( atms : List[AnnotatedTypeMirror] ) : List[(List[Slot], List[Slot])] = {
-    val topToDeclVars = new ListBuffer[ (List[Slot], List[Slot]) ]
-
-    val top    = new ListBuffer[Slot]
-    val nested = new ListBuffer[Slot]
-
-    atms.foreach( atm => {
-      top.clear()
-      nested.clear()
-      listDeclVariables( atm, Some( top ), nested )
-      topToDeclVars += ( top.toList -> nested.toList )
-    })
-
-    topToDeclVars.toList
-  }
-
-  def listDeclVariables(atm : AnnotatedTypeMirror) : List[Slot] = {
-    val variables = new ListBuffer[Slot]
-    listDeclVariables( atm, None, variables )
-    variables.toList
-  }
-
-  /**
-   * This is intended to be used on type-uses not declarations.
-   *
-   * @0 Map<@1 Integer, @2 List<@3 T> > => List(@0, @1, @2, @3) as a list of slots
-   *  //TODO SU2
-   * //TODO: tailrec it by visit node/opposite subtree first and then reverse the order?  Or just create a map of (TopLevel, Position) -> Variable
-   * Do a depth first search of all Variables introduced by the given field ExpressionTree
-   * //TODO: At the moment this is the reverse of the normal depth first search we would do (then it's reversed in
-   * //TODO: listVariables.  I don't think the ordering matters because each variable already has a POS but
-   * //TODO: it might prove important to have the right ordering in the game solver in order to match
-   * //TODO: FieldAccesses and method call wiring correctly, however, I think we should be able to order
-   * //TODO: by pos in worst case
-   * //TODO: Previously we used this only in locations in which we expected no slots
-   * //TODO: That weren't abstract variables, does that matter now?
-   */
-  private def listDeclVariables(atm : AnnotatedTypeMirror, primary : Option[ListBuffer[Slot]], variables : ListBuffer[Slot]) {
-    import scala.collection.JavaConversions._
-
-    def primaryToSlot(atm : AnnotatedTypeMirror) = {
-      Option( slotMgr.extractSlot( atm ) )
-    }
-
-    /**
-     * If primary is defined it means that top-level variables should be placed in the primary buffer
-     * and all others should be placed in the variables list.  Notice, we always pass primary=None to the
-     * recursive listDeclVariables below except for the upper bound on wildcard types
-     * @param slot extracted slot that needs to be added to output
-     * @return
-     */
-    def addVariable( slot : Option[Slot] ) = {
-      if( primary.isDefined ) {
-        primary.get ++= slot
-      } else {
-        variables ++= slot
-      }
-    }
-
-    Option( atm ).map(
-
-      _ match {
-
-        case aat : AnnotatedArrayType =>
-          addVariable( primaryToSlot(aat) )
-          listDeclVariables(aat.getComponentType, None, variables)
-
-        case awt : AnnotatedWildcardType =>
-          listDeclVariables( awt.getExtendsBound, None, variables)
-        //TODO: WILDCARDS DO NOT SEEM TO BE HANDLED APPROPRIATELY
-
-          //listDeclVariables( awt.getSuperBound,   None,    variables )
-          //listDeclVariables( awt.getExtendsBound, primary, variables )
-
-        case atv : AnnotatedTypeVariable =>
-          addVariable( primaryToSlot( atv.getLowerBound ) ) //TODO: CLEARLY NOT RIGHT
-        //TODO SU3: For now, to avoid the bug in upper/lower bounds we do not visit the same variable twice
-
-        case adt : AnnotatedDeclaredType =>
-          addVariable( primaryToSlot(adt) )
-          adt.getTypeArguments.foreach( (typeArg : AnnotatedTypeMirror) => listDeclVariables(typeArg, None, variables) )
-
-        case apt: AnnotatedPrimitiveType =>
-          addVariable( primaryToSlot(apt) )
-
-        case ait : AnnotatedIntersectionType =>
-          addVariable( primaryToSlot(ait) )
-
-        case atm : AnnotatedTypeMirror if atm.isInstanceOf[AnnotatedNoType] |
-                                          atm.isInstanceOf[AnnotatedNullType] =>
-          addVariable( primaryToSlot(atm) ) //This may happen if we visit a constructor return type
-
-        case atm : AnnotatedTypeMirror =>
-          throw new RuntimeException("Unhandled annotated type mirror " + atm.getClass.getCanonicalName)
-      }
-    )
-  }
 
   /**
    * Upper Bounds of type parameters may have multiple parameters (as they have a tree associated with them),
@@ -178,19 +72,43 @@ object SlotUtil {
       return upperBound
     }
 
-    val primaryAnno = slotMgr.extractSlot( atv ).getAnnotation()
+    val primaryAnno = if( atv.getAnnotations.isEmpty ) None else Some( slotMgr.extractSlot( atv ).getAnnotation() )
 
     var upperBound : AnnotatedTypeMirror = atv
     Iterator.continually({ upperBound = getUpperBound( upperBound.asInstanceOf[AnnotatedTypeVariable] ); upperBound })
       .find( bound => !bound.isInstanceOf[AnnotatedTypeVariable] )
       .get
 
-    upperBound.clearAnnotations()
-    upperBound.addAnnotation( primaryAnno )
+    primaryAnno.map( primAnno => {
+      upperBound.clearAnnotations()
+      upperBound.addAnnotation( primAnno )
+    })
+
     upperBound match {
       case ubAtd : AnnotatedDeclaredType     => Right( ubAtd )
       case ubInt : AnnotatedIntersectionType => Left( ubInt )
       case _ => throw new RuntimeException("Unhandled type use upper bound: " + upperBound )
     }
+  }
+
+  def wildcardToUpperBound( wtc : AnnotatedWildcardType ) : Either[AnnotatedIntersectionType, AnnotatedDeclaredType] = {
+
+    val extendsBound = wtc.getExtendsBound
+    val primaryAnno = slotMgr.extractSlot( extendsBound ).getAnnotation()
+
+    def clearAndAdd[ATM <: AnnotatedTypeMirror]( atm : ATM, primaryAnno : AnnotationMirror ) : ATM = {
+      atm.clearAnnotations()
+      atm.addAnnotation( primaryAnno )
+      atm
+    }
+
+    val upperBound = extendsBound match {
+      case extAtd : AnnotatedDeclaredType     => Right( clearAndAdd( extAtd, primaryAnno ) )
+      case extInt : AnnotatedIntersectionType => Left(  clearAndAdd( extInt, primaryAnno ) )
+      case extAtv : AnnotatedTypeVariable     => typeUseToUpperBound( clearAndAdd( extAtv, primaryAnno ) )
+      case _ => throw new RuntimeException("Unhandled type use upper bound: " + extendsBound )
+    }
+
+    return upperBound
   }
 }
