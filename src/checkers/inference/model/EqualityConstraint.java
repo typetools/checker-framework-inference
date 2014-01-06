7f4ffba77536a7b1e921269c0fdf620303be2642
jonathangburke <jonathangburke@gmail.com>
2014-01-06 10:45:02 -0800

    Add comments to Inference Model classes

--- src/checkers/inference/model/EqualityConstraint.java
+++ src/checkers/inference/model/EqualityConstraint.java
@@ -2,6 +2,28 @@ package checkers.inference.model;
 
 import java.util.Arrays;
 
+/**
+ * Represents an equality relationship between two slots.
+ * E.g.
+ *  List<String> ls = new ArrayList<String>();
+ *
+ * If, in any type system:
+ *    //vls represents the variable corresponding to the annotation on the first type String
+ *    //located on the left-hand side of the assignment in List<String> ls ...
+ *    vs = VariableSlot( astPathToVls, 0 )
+ *
+ *    //als represents the variable corresponding to the annotation on the second type String
+ *    //located on the right-hand side of the assignment in ArrayList<String>()
+ *    va = VariableSlot( astPathToAls, 1 )
+ *
+ * Then:
+ *   The above statements would result in the following EqualityConstraints:
+ *   logical representation:           in Java:
+ *   vls == als                        new EqualityConstraint( vls, als )
+ *
+ * Note: The equality relationship is commutative so order should not matter, though in practice
+ * it is up to the given ConstraintSolver to treat them this way.
+ */
 public class EqualityConstraint extends Constraint {
 
     private Slot first;
