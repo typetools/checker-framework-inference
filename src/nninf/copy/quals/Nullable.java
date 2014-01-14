4dada2c1cbee80443b6a8edced4df1282725c16f
David McArthur <mcarthur@cs.washington.edu>
2014-01-14 15:42:51 -0800

    src/nninf/copy/

---
+++ src/nninf/copy/quals/Nullable.java
@@ -0,0 +1,23 @@
+package nninf.copy.quals;
+
+import java.lang.annotation.*;
+
+import nninf.quals.NonNull;
+
+import checkers.quals.SubtypeOf;
+import checkers.quals.TypeQualifier;
+import checkers.quals.ImplicitFor;
+
+import com.sun.source.tree.Tree;
+
+/**
+ * @see NonNull
+ * @see checkers.nullness.quals.Nullable
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE_USE, ElementType.TYPE_PARAMETER })
+@TypeQualifier
+@SubtypeOf({})
+@ImplicitFor(trees = { Tree.Kind.NULL_LITERAL })
+public @interface Nullable {}
