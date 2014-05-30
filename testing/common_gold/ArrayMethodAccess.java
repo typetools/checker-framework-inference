33b7bcc70c07b41e9b10cea46308eeefdc3e6683
David McArthur <mcarthur@cs.washington.edu>
2014-05-30 07:31:30 -0700

    Removing duplicate array test files.

--- testing/common_gold/ArrayMethodAccess.java
+++
@@ -1,8 +0,0 @@
-import checkers.inference.quals.VarAnnot;
-class ArrayMethodAccess {
-	private @VarAnnot(2) String @VarAnnot(1) [] array;
-	
-	public void context() {
-		System.out.println(array.toString());
-	}
-}
