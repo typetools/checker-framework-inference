5f0c4f700e5d226793320a908a0fc45d1e2a5111
Werner Dietl <wdietl@gmail.com>
2015-04-10 00:17:43 -0400

    Two new constraints: to add viewpoint adaptation and to prefer a preference for a particular annotation.

---
+++ src/checkers/inference/model/CombineConstraint.java
@@ -0,0 +1,67 @@
+package checkers.inference.model;
+
+import java.util.Arrays;
+
+/**
+ * Represents a constraint that the viewpoint adaptation between
+ * target and decl gives result.
+ *
+ * TODO: clarify relation to CombVariableSlot. Should we add separate types?
+ */
+public class CombineConstraint extends Constraint {
+
+    private final Slot target;
+    private final Slot decl;
+    private final Slot result;
+
+    public CombineConstraint(Slot target, Slot decl, Slot result) {
+        super(Arrays.asList(target, decl, result));
+        this.target = target;
+        this.decl = decl;
+        this.result = result;
+    }
+
+    @Override
+    public Object serialize(Serializer serializer) {
+        return serializer.serialize(this);
+    }
+
+    public Slot getTarget() {
+        return target;
+    }
+
+    public Slot getDeclared() {
+        return decl;
+    }
+
+    public Slot getResult() {
+        return result;
+    }
+
+    @Override
+    public int hashCode() {
+        int hc = 1;
+        hc += ((target == null) ? 0 : target.hashCode());
+        hc += ((decl == null) ? 0 : decl.hashCode());
+        hc += ((result == null) ? 0 : result.hashCode());
+        return hc;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CombineConstraint other = (CombineConstraint) obj;
+        if (target.equals(other.target) &&
+                decl.equals(other.decl) &&
+                result.equals(other.result)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
