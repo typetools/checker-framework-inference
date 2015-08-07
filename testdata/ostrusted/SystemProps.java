21747b81f0ef0df7f000c76834cabdbc9ea93be6
Jonathan Burke <jonathangburke@gmail.com>
2015-08-07 15:33:27 -0700

    Update test files

---
+++ testdata/ostrusted/SystemProps.java
@@ -0,0 +1,36 @@
+import ostrusted.quals.*;
+import ostrusted.quals.OsUntrusted;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.Runtime;
+
+class SystemProps {
+
+    @OsTrusted String trustedField = "";
+    @OsUntrusted String untrustedField = "";
+    String unknownField = "";
+
+    public void loadContext(String unknownParam, String someParam1, String someParam2) {
+        System.setProperty("prop1", trustedField);
+
+        //:: error: (argument.type.incompatible)
+        System.setProperty("prop2", untrustedField);
+
+        //:: fixable-error: (argument.type.incompatible)
+        System.setProperty("prop3", unknownField);
+
+        System.setProperty("prop4", "some" + "Lib" + "name");
+
+        //:: fixable-error: (argument.type.incompatible)
+        System.setProperty("prop5", unknownParam + "name");
+
+        //:: fixable-error: (argument.type.incompatible)
+        System.setProperty("prop6", someParam1 + someParam2);
+
+        String someLocal1 = "";
+        String someLocal2 = "";
+        System.setProperty("prop7", someLocal1 + someLocal2);
+    }
+
+}
