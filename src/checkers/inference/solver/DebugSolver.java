cb52dcf770bc57bca8f32fe9028be6b00c32136b
Jonathan Burke <jonathangburke@gmail.com>
2015-10-01 13:04:12 -0700

    Update debug solver so that constraints are written together rather than being interrupted by log info

--- src/checkers/inference/solver/DebugSolver.java
+++ src/checkers/inference/solver/DebugSolver.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.List;
 import java.util.LinkedHashMap;
-import java.util.logging.Logger;
 
 import javax.annotation.processing.ProcessingEnvironment;
 
@@ -20,6 +19,7 @@ import checkers.inference.model.Slot;
 import checkers.inference.model.VariableSlot;
 import checkers.inference.model.serialization.ToStringSerializer;
 
+import checkers.inference.util.InferenceUtil;
 import org.checkerframework.framework.type.QualifierHierarchy;
 
 import checkers.inference.InferenceSolver;
@@ -35,6 +35,7 @@ import checkers.inference.InferenceSolver;
 public class DebugSolver implements InferenceSolver {
 
     private static final boolean showAstPaths = true;//System.getProperty("showAstPaths", "false").equalsIgnoreCase("true");
+    private static final int MAX_BUFFER_LENGTH = 10000;
 
     @Override
     public InferenceSolution solve(
@@ -44,28 +45,38 @@ public class DebugSolver implements InferenceSolver {
             QualifierHierarchy qualHierarchy,
             ProcessingEnvironment processingEnvironment) {
 
+        InferenceUtil.flushAllLoggers(true);
+
         final ToStringSerializer serializer = new ToStringSerializer(showAstPaths);
 
         final Map<Class<? extends Slot>, List<Slot>> typesToSlots = partitionSlots(slots);
 
+        StringBuilder stringBuilder = new StringBuilder();
         for(final Entry<Class<? extends Slot>, List<Slot>> entry : typesToSlots.entrySet()) {
             final Class<? extends Slot> type = entry.getKey();
             final List<Slot> slotsForType = entry.getValue();
 
-            System.out.println();
-            System.out.println("Created " + type.getSimpleName());
+            stringBuilder.append("\n");
+            stringBuilder.append("Created " + type.getSimpleName());
             serializer.setIndent(1);
-            System.out.println(serializer.serializeSlots(slotsForType, "\n"));
+            stringBuilder.append(serializer.serializeSlots(slotsForType, "\n"));
             serializer.setIndent(0);
+
+            if (stringBuilder.length() > MAX_BUFFER_LENGTH) {
+                System.out.print(stringBuilder.toString());
+                stringBuilder = new StringBuilder();
+            }
         }
 
-        System.out.println();
-        System.out.println("Created these constraints:");
+        stringBuilder.append("\n");
+        stringBuilder.append("Created these constraints:");
 
         serializer.setIndent(1);
-        System.out.print(serializer.serializeConstraints(constraints, "\n"));
+        stringBuilder.append(serializer.serializeConstraints(constraints, "\n"));
         serializer.setIndent(0);
-        System.out.println();
+        stringBuilder.append("\n");
+
+        System.out.print(stringBuilder.toString());
         System.out.flush();
 
         return null;
