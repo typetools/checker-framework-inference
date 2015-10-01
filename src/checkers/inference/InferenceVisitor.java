b469892b4bbf0c04ebc256df150698de25a6cda0
Jonathan Burke <jonathangburke@gmail.com>
2015-10-01 12:24:30 -0700

    Add Javadoc to Inference Visitor

--- src/checkers/inference/InferenceVisitor.java
+++ src/checkers/inference/InferenceVisitor.java
@@ -59,7 +59,18 @@ import com.sun.source.tree.VariableTree;
 import static org.checkerframework.framework.util.AnnotatedTypes.findEffectiveAnnotationInHierarchy;
 
 /**
- * Created by jburke on 3/6/15.
+ *  InferenceVisitor visits trees in each compilation unit both in typecheck/inference mode.
+ *  In typecheck mode, it functions nearly identically to BaseTypeVisitor, i.e. it
+ *  enforces common assignment and other checks.  However, it also defines a new
+ *  API that may be more intuitive for checker writers (see mainIsNot).
+ *
+ *  InferneceVisitor has an "infer" flag which indicates whether or not
+ *  it is in typecheck or in inference mode.  When true, this class replaces type checks
+ *  with constraint generation.
+ *
+ *  InferneceVisitor is intended to replace BaseTypeVisitor.
+ *  That is, the methods from BaseTypeVisiotr should be migrated here and InferenceVisitor
+ *  should replace it in the Visitor hierarchy.
  */
 public class InferenceVisitor<Checker extends InferenceChecker,
         Factory extends BaseAnnotatedTypeFactory>
