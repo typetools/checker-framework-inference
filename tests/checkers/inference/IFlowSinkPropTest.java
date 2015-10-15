3baf87075045e4879323264b9fd00fc6fd599432
Suzanne Millstein <smillst@cs.washington.edu>
2015-10-15 14:05:51 -0700

    Remove sparta from more locations

--- tests/checkers/inference/IFlowSinkPropTest.java
+++ tests/checkers/inference/IFlowSinkPropTest.java
@@ -4,30 +4,30 @@ import checkers.inference.test.CFInferenceTest;
 import org.checkerframework.framework.test.TestUtilities;
 import org.checkerframework.javacutil.Pair;
 import org.junit.runners.Parameterized.Parameters;
-import sparta.checkers.IFlowSourceChecker;
-import sparta.checkers.propagation.IFlowSourceSolver;
+import sparta.checkers.IFlowSinkChecker;
+import sparta.checkers.propagation.IFlowSinkSolver;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SpartaSourceTest extends CFInferenceTest {
+public class IFlowSinkPropTest extends CFInferenceTest {
 
-    public SpartaSourceTest(File testFile) {
-        super(testFile,  IFlowSourceChecker.class, "sparta"+File.separator+"checkers",
+    public IFlowSinkPropTest(File testFile) {
+        super(testFile,  IFlowSinkChecker.class, "sparta"+File.separator+"checkers",
                 "-Anomsgtext",  "-Astubs=src/sparta/checkers/information_flow.astub", "-d", "tests/build/outputdir");
     }
 
     @Override
     public Pair<String, List<String>> getSolverNameAndOptions() {
-        return Pair.<String, List<String>>of(IFlowSourceSolver.class.getCanonicalName(), new ArrayList<String>());
+        return Pair.<String, List<String>>of(IFlowSinkSolver.class.getCanonicalName(), new ArrayList<String>());
     }
 
     @Parameters
     public static List<File> getTestFiles(){
         List<File> testfiles = new ArrayList<>();//InferenceTestUtilities.findAllSystemTests();
         TestUtilities.filterOutJdk8Sources(testfiles);
-        testfiles.addAll(TestUtilities.findRelativeNestedJavaFiles("testdata", "sparta-source"));
+        testfiles.addAll(TestUtilities.findRelativeNestedJavaFiles("testdata", "iflowsink"));
         return testfiles;
     }
 }
--- tests/checkers/inference/IFlowSourcePropTest.java
+++ tests/checkers/inference/IFlowSourcePropTest.java
@@ -4,30 +4,30 @@ import checkers.inference.test.CFInferenceTest;
 import org.checkerframework.framework.test.TestUtilities;
 import org.checkerframework.javacutil.Pair;
 import org.junit.runners.Parameterized.Parameters;
-import sparta.checkers.IFlowSinkChecker;
-import sparta.checkers.propagation.IFlowSinkSolver;
+import sparta.checkers.IFlowSourceChecker;
+import sparta.checkers.propagation.IFlowSourceSolver;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SpartaSinkTest extends CFInferenceTest {
+public class IFlowSourcePropTest extends CFInferenceTest {
 
-    public SpartaSinkTest(File testFile) {
-        super(testFile,  IFlowSinkChecker.class, "sparta"+File.separator+"checkers",
+    public IFlowSourcePropTest(File testFile) {
+        super(testFile,  IFlowSourceChecker.class, "sparta"+File.separator+"checkers",
                 "-Anomsgtext",  "-Astubs=src/sparta/checkers/information_flow.astub", "-d", "tests/build/outputdir");
     }
 
     @Override
     public Pair<String, List<String>> getSolverNameAndOptions() {
-        return Pair.<String, List<String>>of(IFlowSinkSolver.class.getCanonicalName(), new ArrayList<String>());
+        return Pair.<String, List<String>>of(IFlowSourceSolver.class.getCanonicalName(), new ArrayList<String>());
     }
 
     @Parameters
     public static List<File> getTestFiles(){
         List<File> testfiles = new ArrayList<>();//InferenceTestUtilities.findAllSystemTests();
         TestUtilities.filterOutJdk8Sources(testfiles);
-        testfiles.addAll(TestUtilities.findRelativeNestedJavaFiles("testdata", "sparta-sink"));
+        testfiles.addAll(TestUtilities.findRelativeNestedJavaFiles("testdata", "iflowsource"));
         return testfiles;
     }
 }
