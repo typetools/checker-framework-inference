2cdf7bc2f391e447bca57649f6b9d20de87bacce
Suzanne Millstein <smillst@cs.washington.edu>
2015-10-15 14:18:53 -0700

    Added tests for the IFlow SAT solvers.

---
+++ tests/checkers/inference/IFlowSinkSatTest.java
@@ -0,0 +1,33 @@
+package checkers.inference;
+
+import checkers.inference.test.CFInferenceTest;
+import org.checkerframework.framework.test.TestUtilities;
+import org.checkerframework.javacutil.Pair;
+import org.junit.runners.Parameterized.Parameters;
+import sparta.checkers.IFlowSinkChecker;
+import sparta.checkers.sat.SinkSolver;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IFlowSinkSatTest extends CFInferenceTest {
+
+    public IFlowSinkSatTest(File testFile) {
+        super(testFile,  IFlowSinkChecker.class, "sparta"+File.separator+"checkers",
+                "-Anomsgtext",  "-Astubs=src/sparta/checkers/information_flow.astub", "-d", "tests/build/outputdir");
+    }
+
+    @Override
+    public Pair<String, List<String>> getSolverNameAndOptions() {
+        return Pair.<String, List<String>>of(SinkSolver.class.getCanonicalName(), new ArrayList<String>());
+    }
+
+    @Parameters
+    public static List<File> getTestFiles(){
+        List<File> testfiles = new ArrayList<>();//InferenceTestUtilities.findAllSystemTests();
+        TestUtilities.filterOutJdk8Sources(testfiles);
+        testfiles.addAll(TestUtilities.findRelativeNestedJavaFiles("testdata", "iflowsink"));
+        return testfiles;
+    }
+}
