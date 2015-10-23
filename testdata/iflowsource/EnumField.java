78f70d3788d3e3d5cacd8b9f615e31d435e58264
Suzanne Millstein <smillst@cs.washington.edu>
2015-10-23 14:38:38 -0700

    Implemented the isSubtype method correctly for the IFlow Source and Sink Checkers Updated the tests with "fixable" errors.

--- testdata/iflowsource/EnumField.java
+++ testdata/iflowsource/EnumField.java
@@ -2,7 +2,8 @@ import sparta.checkers.quals.*;
 public enum EnumField{
     CONST("hello");
     public String enumField;
-    private EnumField(@Source("READ_SMS") @Sink({}) String field){
+    private @Source({}) EnumField(@Source("READ_SMS") @Sink({}) String field){
+       //:: fixable-error: (assignment.type.incompatible)
        this.enumField = field;
    }
 }
--- testdata/iflowsource/PolySourceTest.java
+++ testdata/iflowsource/PolySourceTest.java
@@ -5,11 +5,13 @@ public class PolySourceTest{
 //@PolySource String poly( @PolySource String param) { return param; }
 int readSmsField;
 void foo(@Source("READ_SMS") int readSMS){
+   //:: fixable-error: (assignment.type.incompatible)
    readSmsField = Math.abs(readSMS);
    bar(Math.abs(readSMS));
    testReadSms(readSmsField);
 }
 void bar(int test){
+   //:: fixable-error: (argument.type.incompatible)
     testReadSms(test);
 }
 
