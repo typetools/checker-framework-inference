01701e230cdf5e9d7a75a67dfa298a79e1d73af9
Werner Dietl <wdietl@gmail.com>
2015-11-17 18:31:58 -0500

    Adapt to combined CnfVecIntSerializer. Simplify.

--- src/sparta/checkers/sat/IFlowSerializer.java
+++ src/sparta/checkers/sat/IFlowSerializer.java
@@ -1,20 +1,17 @@
 package sparta.checkers.sat;
 
 import checkers.inference.InferenceMain;
-import checkers.inference.SlotManager;
-import sparta.checkers.CnfVecIntSerializer;
+import checkers.inference.model.serialization.CnfVecIntSerializer;
 import sparta.checkers.iflow.util.PFPermission;
 
 /**
  * Created by smillst on 9/16/15.
  */
 public abstract class IFlowSerializer extends CnfVecIntSerializer {
-    private SlotManager slotManager;
     protected PFPermission permission;
 
     public IFlowSerializer(PFPermission permission) {
         super(InferenceMain.getInstance().getSlotManager());
-        this.slotManager = InferenceMain.getInstance().getSlotManager();
         this.permission = permission;
     }
 }
