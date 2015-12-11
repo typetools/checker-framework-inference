62d881924c864bbb3efafb80e42bc9a1859efda6
Michael Ernst <mernst@cs.washington.edu>
2015-12-11 14:27:49 -0800

    Don't run Checker Framework tests when just building it

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 ROOT=$TRAVIS_BUILD_DIR/..
-cd $ROOT
-git clone https://github.com/typetools/checker-framework.git
-cd checker-framework/
-./.travis-build.sh
-# This also builds annotation-tools and jsr308-langtools
 
-cd $ROOT/checker-framework-inference
+## Build Checker Framework
+(cd $ROOT && git clone https://github.com/typetools/checker-framework.git)
+# This also builds annotation-tools and jsr308-langtools
+(cd checker-framework/ && ./.travis-build-without-test.sh)
+
 gradle dist
 gradle copytest
 ant -f tests.xml run-tests
