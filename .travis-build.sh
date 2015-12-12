7ca20b33f3d0127a18ef04d62d24e70905f7ad2a
Michael Ernst <mernst@cs.washington.edu>
2015-12-11 18:52:26 -0800

    Don't ignore errors in shell scripts

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 ROOT=$TRAVIS_BUILD_DIR/..
 
+# Fail the whole script if any command fails
+set -e
+
 ## Build Checker Framework
 (cd $ROOT && git clone https://github.com/typetools/checker-framework.git)
 # This also builds annotation-tools and jsr308-langtools
