33c6f21dd7a3fb8c17dc3667a5c05db157d8e3ef
Werner Dietl <wdietl@gmail.com>
2016-01-17 15:54:27 -0500

    Remove dead code.

--- src/checkers/inference/InferenceChecker.java
+++ src/checkers/inference/InferenceChecker.java
@@ -1,18 +1,9 @@
 package checkers.inference;
 
-import java.lang.annotation.Annotation;
-import java.util.Arrays;
-import java.util.List;
-
 import org.checkerframework.common.basetype.BaseTypeChecker;
 import org.checkerframework.common.basetype.BaseTypeVisitor;
-import org.checkerframework.framework.qual.Unqualified;
-
-import checkers.inference.quals.VarAnnot;
 
 public class InferenceChecker extends BaseTypeChecker {
-
-    private static List<Class<? extends Annotation>> INFERENCE_ANNOTATIONS = Arrays.asList(VarAnnot.class, Unqualified.class);
 
     @Override
     public void initChecker() {
@@ -21,10 +12,6 @@ public class InferenceChecker extends BaseTypeChecker {
         super.initChecker();
         // Overrides visitor created by initChecker
         this.visitor = InferenceMain.getInstance().getVisitor();
-    }
-
-    public static List<Class<? extends Annotation>> getInferenceAnnotations() {
-       return INFERENCE_ANNOTATIONS;
     }
 
     /**
