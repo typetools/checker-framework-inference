3519bb52d64c2628cd0c6019f21167d7dd6caedc
Jianchu <jianchuli0624@gmail.com>
2016-04-18 16:55:10 -0400

    issue24

---
+++ testdata/ostrusted/Issue24.java
@@ -0,0 +1,11 @@
+//@skip-test
+import java.util.Collections;
+import java.util.List;
+
+public class Issue24 {
+	
+    public void sort(List<Integer> unsorted) {
+        Collections.sort(unsorted);
+    }
+
+}
