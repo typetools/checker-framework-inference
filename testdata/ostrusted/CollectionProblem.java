9dbce191cbb0bbcfcf0ebd2b9a9b8b0d729de6b1
Jianchu <jianchuli0624@gmail.com>
2016-04-18 14:57:24 -0400

    add collection problem

---
+++ testdata/ostrusted/CollectionProblem.java
@@ -0,0 +1,11 @@
+//@skip-test
+import java.util.Collections;
+import java.util.List;
+
+public class CollectionProblem {
+	
+	public void sort(List<Integer> unsorted) {
+		Collections.sort(unsorted);
+	}
+
+}
