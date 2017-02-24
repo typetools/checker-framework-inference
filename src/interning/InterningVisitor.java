2094dfc8dcdb867c9945147eb4292530ce0cbf17
Suzanne Millstein <smillst@cs.washington.edu>
2017-02-24 13:04:30 -0800

    Adapt to Checker Framework change

--- src/interning/InterningVisitor.java
+++ src/interning/InterningVisitor.java
@@ -246,7 +246,7 @@ public final class InterningVisitor extends InferenceVisitor<InterningChecker, B
      * @see org.checkerframework.common.basetype.BaseTypeVisitor#visitClass(com.sun.source.tree.ClassTree, java.lang.Object)
      */
     @Override
-    public Void visitClass(ClassTree node, Void p) {
+    public void processClassTree(ClassTree node) {
         // TODO: Should this method use the Javac types or some other utility to get
         // all direct supertypes instead, and should it verify that each does not
         // override .equals and that at least one of them is annotated with @UsesObjectEquals?
@@ -288,7 +288,7 @@ public final class InterningVisitor extends InferenceVisitor<InterningChecker, B
             }
         }
 
-        return super.visitClass(node, p);
+        super.processClassTree(node);
     }
 
     // **********************************************************************
