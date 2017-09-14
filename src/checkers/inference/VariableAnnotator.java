df26aa6c5fa9fceea09912c47ae9a4991aeddc74
Werner Dietl <wdietl@gmail.com>
2017-09-14 16:48:55 -0400

    Revert "Avoid NPE (#59)"
    This reverts commit cf5e1efc9fd04a26db4a6b78df375df444c1f248.

--- src/checkers/inference/VariableAnnotator.java
+++ src/checkers/inference/VariableAnnotator.java
@@ -1615,7 +1615,7 @@ public class VariableAnnotator extends AnnotatedTypeScanner<Void,Tree> {
 
             @Override
             protected Boolean scan(AnnotatedTypeMirror type, Void aVoid) {
-                if (type != null && InferenceQualifierHierarchy.findVarAnnot(type.getAnnotations()) != null) {
+                if (InferenceQualifierHierarchy.findVarAnnot(type.getAnnotations()) != null) {
                     return true;
                 }
                 Boolean superCall = super.scan(type, aVoid);
--- src/checkers/inference/util/ConstantToVariableAnnotator.java
+++ src/checkers/inference/util/ConstantToVariableAnnotator.java
@@ -48,7 +48,7 @@ public class ConstantToVariableAnnotator extends AnnotatedTypeScanner<Void, Void
     @Override
     protected Void scan(AnnotatedTypeMirror type, Void aVoid) {
 
-        if (type != null && !type.getAnnotations().isEmpty()) {
+        if (!type.getAnnotations().isEmpty()) {
             addVariablePrimaryAnnotation(type);
         }
         super.scan(type, null);
