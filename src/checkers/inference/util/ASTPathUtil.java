d12735fbeacd590fb11a438845ad2f4cebfbde89
Michael Ernst <mernst@cs.washington.edu>
2017-11-27 08:11:28 -0800

    Adapt to scene-lib package name change

--- src/checkers/inference/util/ASTPathUtil.java
+++ src/checkers/inference/util/ASTPathUtil.java
@@ -24,9 +24,9 @@ import com.sun.source.tree.Tree;
 import com.sun.source.tree.Tree.Kind;
 import com.sun.source.util.TreePath;
 
-import annotations.io.ASTIndex;
-import annotations.io.ASTPath;
-import annotations.io.ASTRecord;
+import scenelib.annotations.io.ASTIndex;
+import scenelib.annotations.io.ASTPath;
+import scenelib.annotations.io.ASTRecord;
 
 /**
  * ASTPathUtil is a collection of utilities to create ASTRecord for existing trees, as well
