957bc3759bbbf51808cc8f29c00f857ffbf5d666
Jeff Luo <jyluo@users.noreply.github.com>
2017-12-14 13:51:30 -0500

    Fix JaifBuilder not outputting String annotation fields correctly (#122)
    * Fix JaifBuilder not outputting String annotation fields correctly
    
    * Add TODO comment about rewriting
    
    * Swap "java.lang.String" with class literal

--- src/checkers/inference/util/JaifBuilder.java
+++ src/checkers/inference/util/JaifBuilder.java
@@ -110,6 +110,9 @@ public class JaifBuilder {
      * @return the header
      */
     private String buildAnnotationHeader(Class<? extends Annotation> annotation) {
+        // TODO: rewrite this method from scratch. We don't account for all possible cases of
+        // outputs at this moment (eg annotation-field arrays). A clean rewrite should
+        // exercise and be tested for all valid annotation field types.
         String result = "";
         String packageName = annotation.getPackage().toString();
         result += packageName + ":\n";
@@ -123,6 +126,9 @@ public class JaifBuilder {
                 }
                 if (method.getReturnType().isArray()) {
                     result += method.getReturnType().getComponentType().getSimpleName() + "[]";
+                } else if (method.getReturnType().isPrimitive() || method.getReturnType()
+                        .getCanonicalName().contentEquals(String.class.getCanonicalName())) {
+                    result += method.getReturnType().getSimpleName();
                 } else {
                     result += method.getReturnType().getCanonicalName();
                 }
