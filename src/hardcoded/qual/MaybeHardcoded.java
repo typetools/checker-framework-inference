a97910f36617b67dc0cd50a054be275209a02bcc
Werner Dietl <wdietl@gmail.com>
2018-01-30 17:40:32 -0500

    Adapt to upstream change for #1781. (#68)

--- src/hardcoded/qual/MaybeHardcoded.java
+++ src/hardcoded/qual/MaybeHardcoded.java
@@ -4,14 +4,13 @@ import org.checkerframework.framework.qual.DefaultQualifierInHierarchy;
 import org.checkerframework.framework.qual.ImplicitFor;
 import org.checkerframework.framework.qual.LiteralKind;
 import org.checkerframework.framework.qual.SubtypeOf;
+import org.checkerframework.framework.qual.TypeKind;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
-import javax.lang.model.type.TypeKind;
 
 /**
  * Represents data that may have been hardcoded.
