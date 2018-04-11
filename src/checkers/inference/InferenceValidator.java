1ad3ee88b7292cd3a2b4197006a3ec697b93ba2b
Mier Ta <topnessman@sina.com>
2018-04-11 10:46:39 -0400

    Add Javadoc for infer field

--- src/checkers/inference/InferenceValidator.java
+++ src/checkers/inference/InferenceValidator.java
@@ -10,6 +10,14 @@ import org.checkerframework.framework.type.AnnotatedTypeFactory;
  */
 public class InferenceValidator extends BaseTypeValidator {
 
+    /**
+     * Indicates whether the validator is in inference mode or not.
+     *
+     * This field is intended to make implementations of subclasses easier.
+     * Instead of querying the {@link InferenceVisitor#infer}, subclasses can
+     * directly query this field to decide whether to generate constraints or
+     * perform typechecking.
+     */
     protected boolean infer;
 
     public InferenceValidator(BaseTypeChecker checker,
