83d11ff757ffd861470cf66d1e2c21a98c326996
Michael Ernst <mernst@cs.washington.edu>
2018-08-04 09:13:46 -0700

    Add missing backticks

--- .travis-build-without-test.sh
+++ .travis-build-without-test.sh
@@ -23,11 +23,11 @@ if [ -d ../checker-framework ] ; then
   git -C ../checker-framework pull
 else
   (cd .. && git clone --depth 1 https://github.com/plume-lib/plume-scripts.git)
-  REPO=../plume-scripts/git-find-repo ${SLUGOWNER} typetools checker-framework
+  REPO=`../plume-scripts/git-find-repo ${SLUGOWNER} typetools checker-framework`
   echo "REPO=$REPO"
   echo "TRAVIS_PULL_REQUEST_BRANCH=$TRAVIS_PULL_REQUEST_BRANCH"
   echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
-  BRANCH=../plume-scripts/git-find-branch $REPO ${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}
+  BRANCH=`../plume-scripts/git-find-branch $REPO ${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}`
   echo "BRANCH=$BRANCH"
   (cd .. && git clone -b $BRANCH --single-branch --depth 1 $REPO) || (cd .. && git clone -b $BRANCH --single-branch --depth 1 $REPO)
 fi
