4614b92ff17238eb98018d6b83ea798fc94f190f
Michael Ernst <mernst@cs.washington.edu>
2018-08-09 07:32:12 -0700

    Move location for plume-scripts

--- .travis-build-without-test.sh
+++ .travis-build-without-test.sh
@@ -22,9 +22,9 @@ fi
 if [ -d ../checker-framework ] ; then
     git -C ../checker-framework pull
 else
-    (cd .. && git clone --depth 1 https://github.com/plume-lib/plume-scripts.git)
-    REPO=`../plume-scripts/git-find-fork ${SLUGOWNER} typetools checker-framework`
-    BRANCH=`../plume-scripts/git-find-branch ${REPO} ${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}`
+    [ -d /tmp/plume-scripts ] || (cd /tmp && git clone --depth 1 https://github.com/plume-lib/plume-scripts.git)
+    REPO=`/tmp/plume-scripts/git-find-fork ${SLUGOWNER} typetools checker-framework`
+    BRANCH=`/tmp/plume-scripts/git-find-branch ${REPO} ${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}`
     echo "About to execute: (cd .. && git clone -b $BRANCH --single-branch --depth 1 $REPO)"
     (cd .. && git clone -b ${BRANCH} --single-branch --depth 1 ${REPO}) || (cd .. && git clone -b ${BRANCH} --single-branch --depth 1 ${REPO})
 fi
