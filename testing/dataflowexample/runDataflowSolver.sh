4607a2b1b03db9b3ac580dc997f494c4ca3f52f4
Jeff Luo <j36luo@uwaterloo.ca>
2018-09-18 00:05:41 -0400

    add travis debug output

--- testing/dataflowexample/runDataflowSolver.sh
+++ testing/dataflowexample/runDataflowSolver.sh
@@ -6,10 +6,13 @@ JSR308=$(cd $WORKING_DIR/../../../ && pwd)
 
 DLJC=$JSR308/do-like-javac
 
-( cd $WORKING_DIR && \
+cd $WORKING_DIR
+
 $DLJC/dljc -t inference \
     --guess --crashExit \
     --checker dataflow.DataflowChecker \
     --solver dataflow.solvers.classic.DataflowSolver \
     --mode ROUNDTRIP -o $WORKING_DIR/logs \
-    -afud $WORKING_DIR/annotated -- ant compile-project )
+    -afud $WORKING_DIR/annotated -- ant compile-project
+
+cat ./logs/infer.log
