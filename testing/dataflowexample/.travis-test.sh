16d39dc007b3cac7963009763befd806d9f89c27
Jeff Luo <j36luo@uwaterloo.ca>
2018-09-18 00:28:49 -0400

    add paths for AFU and Lingeling

--- testing/dataflowexample/.travis-test.sh
+++ testing/dataflowexample/.travis-test.sh
@@ -8,6 +8,11 @@ JSR308=$(cd $WORKING_DIR/../../../ && pwd)
 
 $WORKING_DIR/dataflow-setup.sh
 
+export AFU=$JSR308/annotation-tools/annotation-file-utilities
+export LINGELING=$JSR308/lingeling
+
+export PATH=$LINGELING:$AFU/scripts:$PATH
+
 # test using basic dataflow solver
 $WORKING_DIR/runDataflowSolver.sh
 $WORKING_DIR/cleanup.sh
