cb33413572d746afe7ff74d9af3a9e90d0611778
Michael Ernst <mernst@cs.washington.edu>
2019-02-14 15:41:02 -0800

    Use TRAVIS_PULL_REQUEST_SLUG if available

--- .travis-build-without-test.sh
+++ .travis-build-without-test.sh
@@ -16,7 +16,10 @@ export CHECKERFRAMEWORK=`readlink -f ${CHECKERFRAMEWORK:-../checker-framework}`
 export PATH=$AFU/scripts:$JAVA_HOME/bin:$PATH
 
 ## Build Checker Framework
-SLUGOWNER=${TRAVIS_REPO_SLUG%/*}
+SLUGOWNER=${TRAVIS_PULL_REQUEST_SLUG%/*}
+if [[ "$SLUGOWNER" == "" ]]; then
+  SLUGOWNER=${TRAVIS_REPO_SLUG%/*}
+fi
 if [[ "$SLUGOWNER" == "" ]]; then
   SLUGOWNER=typetools
 fi
