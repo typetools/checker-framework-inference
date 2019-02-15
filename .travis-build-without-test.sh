c71fc1305f36f3d4e746df8c29f671cf427cbe8e
Michael Ernst <mernst@cs.washington.edu>
2019-02-14 16:08:34 -0800

    Typo fix, remove debugging output

--- .travis-build-without-test.sh
+++ .travis-build-without-test.sh
@@ -16,19 +16,14 @@ export CHECKERFRAMEWORK=`readlink -f ${CHECKERFRAMEWORK:-../checker-framework}`
 export PATH=$AFU/scripts:$JAVA_HOME/bin:$PATH
 
 ## Build Checker Framework
-echo TRAVIS_PULL_REQUEST_SLUG=$TRAVIS_PULL_REQUEST_SLUG
 SLUGOWNER=${TRAVIS_PULL_REQUEST_SLUG%/*}
-echo SLUGOWNER=$SLUGOWNER
 if [[ "$SLUGOWNER" == "" ]]; then
-  echo TRAVIS_REPO_SLUG=$TRAVIS_REPO_SLUG
   SLUGOWNER=${TRAVIS_REPO_SLUG%/*}
-  echo SLUGOWNER=$SLUGOWNER
 fi
 if [[ "$SLUGOWNER" == "" ]]; then
-  echo SLUGOWNER is blank
   SLUGOWNER=typetools
-  echo SLUGOWNER=$SLUGOWNER
 fi
+echo SLUGOWNER=$SLUGOWNER
 
 ## Build Checker Framework
 if [ -d $CHECKERFRAMEWORK ] ; then
