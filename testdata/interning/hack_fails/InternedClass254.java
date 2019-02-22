c44dc98f2b6655b31903e3d10e419bbdd7d63fda
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2019-02-22 11:05:31 -0800

    Revert "Added expected error to interning test introduced after fixing #2264 (#100)"
    This reverts commit 0c7b7bef543e1e0e29fd83257d3a34a6cc23aa6b.

--- testdata/interning/hack_fails/InternedClass254.java
+++ testdata/interning/hack_fails/InternedClass254.java
@@ -1,6 +1,5 @@
 import interning.qual.Interned;
 
-// :: error: (super.invocation.invalid)
 @Interned class InternedClass {
     void foo(Object other) {
         boolean b = other instanceof InternedClass;
